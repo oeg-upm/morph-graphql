cf91d61ef7192a393fa5ab4df4128dbde826ec36
Ahmad Alobaid <ahmad88me@gmail.com>
2018-08-22 20:26:24 +0200

    add structure of transform

--- main.js
+++ main.js
@@ -7,11 +7,54 @@
 //     res.end();
 // }).listen(8082);
 
-
-
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) =>
+  res.send('Hello World!'))
+
+app.post('/transform', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  //prog_lang
+  //map_lang
+  //dataset_type
+  if(req.body.map_lang && req.body.prog_lang && req.body.dataset_type){
+    //call the transform function
+    //res.send(JSON.stringify({ "msg": "parameters are passed successfully" }))
+    if(req.body.mapping_url){
+      res.json({"msg": "the url is passed"})
+    }
+    else{
+      res.json({"error": "the mapping_url is not passed"})
+    }
+  }
+  else{
+    res.json({ "error": "parameters are not passed" });
+    //res.send(JSON.stringify({"error":'expecting map_lang, prog_lang, and dataset_type' }))
+  }
+})
+
+
+function transform(prog_lang, map_lang, dataset_type){
+  create_schema(prog_lang, map_lang, dataset_type)
+  create_resolver(prog_lang, map_lang, dataset_type)
+}
+
+function   create_resolver(prog_lang, map_lang, dataset_type){
+  
+}
+
+
+function create_schema(prog_lang, map_lang, dataset_type){
+
+}
+
+
+
+
+
 
 app.listen(8082, () => console.log('Example app listening on port 8082!'))

b43399a41568f7adbbe65f6fe8bd67738ebcbd35
Ahmad Alobaid <ahmad88me@gmail.com>
2018-08-23 19:44:27 +0200

    hierarchy for parsing the mapping

--- app.js
+++ app.js
@@ -38,54 +38,109 @@ app.get('/testFreddy', function (req, res) {
   res.render('transform', {message: 'Hello there from Freddy!' })
 })
 
+app.get('/transform', function (req, res){
+    res.render('transform', {message: 'Hello there from Ahmad!' })
+})
+
 app.post('/transform', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
-  //prog_lang
-  //map_lang
-  //dataset_type
-  if(req.body.map_lang && req.body.prog_lang && req.body.dataset_type){
-    //call the transform function
-    //res.send(JSON.stringify({ "msg": "parameters are passed successfully" }))
-    if(req.body.mapping_url){
-      res.json({"msg": "the url is passed"})
-    }
-    else{
-      res.json({"error": "the mapping_url is not passed"})
-    }
-  }
-  else{
-    res.json({ "error": "parameters are not passed" });
-    //res.send(JSON.stringify({"error":'expecting map_lang, prog_lang, and dataset_type' }))
-  }
+      if(req.body.prog_lang && req.body.dataset_type && req.body.mapping_url){
+         tranform(req.body.prog_lang, "r2rml", req.body.dataset_type, req.body.mapping_url)
+      }
+      else{
+        res.json({ "error": "parameters are not passed" });
+        //res.send(JSON.stringify({"error":'expecting map_lang, prog_lang, and dataset_type' }))
+      }
 })
 
 
-function transform(prog_lang, map_lang, dataset_type){
-  create_schema(prog_lang, map_lang, dataset_type)
-  create_resolver(prog_lang, map_lang, dataset_type)
+function transform(prog_lang, map_lang, dataset_type, mapping_url){
+  create_resolver(prog_lang, map_lang, dataset_type, mapping_url)
 }
 
-function get_jsonld_from_mapping(){
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://rdf-translator.appspot.com/convert/n3/json-ld/https://raw.githubusercontent.com/oeg-upm/mapping-translator/master/example/43LR95CY57.rml.ttl", false);
-    xhttp.send();
-    console.log('reply: ');
-    //console.log(xhttp.responseText);
-    var j = JSON.parse(xhttp.responseText);
-    var i;
-    var item
+
+function get_class_name(j){
+    var model_name
     for(i=0;i<j["@graph"].length;i++){
         item = j["@graph"][i];
-        if("rr:class" in item){
+        if("rr:class" in item){'
             con_arr = item["rr:class"]["@id"].split(":")
             model_name =  con_arr[con_arr.length-1]
             console.log("model name: "+model_name)
-        }
-        else{
-            
+            break
         }
     }
+    return model_name
+}
+
+function get_logical_source(j){
+    //table
+}
+
+function get_predicate_object_map_list(j){
+}
+
+function get_predicate(predicate_object_map){
+    
+}
+
+function get_jsonld_from_mapping(mapping_url){
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://rdf-translator.appspot.com/convert/n3/json-ld/"+mapping_url, false);
+    xhttp.send();
+    console.log('reply: ');
+    var j = JSON.parse(xhttp.responseText);
+    var i;
+    var item, new_item
+    var k, keys
+    //var subjectMap_id
+    //var class_name
+    var graph
+    get_class_name(j)
+
+    
+
+    
+    
+    for(i=0;i<j["@graph"].length;i++){
+        item = j["@graph"][i];
+        if("rr:class" in item){'
+            //            class_name = item["rr:class"]["@id"].split(":")
+            con_arr = item["rr:class"]["@id"].split(":")
+            model_name =  con_arr[con_arr.length-1]
+            console.log("model name: "+model_name)
+            //data[model_name]=[]
+            break
+        }
+    }
+
+    var pred_obj_map_ids = []
+    for(i=0;i<j["@graph"].length;i++){
+        item = j["@graph"][i];
+        if("rr:predicateObjectMap" in item){
+            pred_obj_map_ids.push(item["rr:predicateObjectMap"]["@id"]
+        }
+    }
+    var pred_obj_map_list = []
+    for(i=0;i<j["@graph"].length;i++){
+        item = j["@graph"][i];
+         for(k=0;k<pred_obj_map_ids.length;k++){
+              if(item["id@"]==pred_obj_map_ids[k]){
+                    pred_obj_map_list.push({"predicate",""})
+                                  
+                                  
+                                  "rr:objectMap": {
+                                  "@id": "_:ub182bL32C27"
+                                  },
+                                  "rr:predicate": {
+                                  "@id": "schema:name"
+                                  }
+                                  
+              }
+        }
+    }
+    
 }
 
 function create_resolver(prog_lang, map_lang, dataset_type, mapping_data){

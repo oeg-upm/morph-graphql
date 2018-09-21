81d03cafeb0507515fccdce79d2168bdba584c1f
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-09-21 11:28:33 +0200

    update example

--- example/sqlite/personas.sql
+++ example/sqlite/personas.sql
@@ -1,3 +1,3 @@
 CREATE TABLE Personas(id VARCHAR, nombre VARCHAR, correo VARCHAR, PRIMARY KEY(id));
 INSERT INTO Personas VALUES("1", 'Freddy', 'fpriyatna@fi.upm.es');
-INSERT INTO Personas VALUES("2", 'Ahmad', 'dchaves@fi.upm.es');
+INSERT INTO Personas VALUES("2", 'Ahmad', 'aalobaid@fi.upm.es');

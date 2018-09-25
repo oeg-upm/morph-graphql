d334f8fecaa3f5e89f5b41fe88c4daa2d3a6e5f2
Freddy Priyatna <fpriyatna@fi.upm.es>
2018-09-25 16:50:21 +0200

    add example3 database

---
+++ example/sqlite/personas3.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Personas(id VARCHAR, nombre VARCHAR, apellido VARCHAR, PRIMARY KEY(id));
+INSERT INTO Personas VALUES("1", 'Freddy', 'Priyatna');
+INSERT INTO Personas VALUES("2", 'Ahmad', 'Alobaid');
+SELECT * FROM Personas;
